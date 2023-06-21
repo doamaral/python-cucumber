aa5e0df37e977215ca43d4be82c43f5a0be72d6d
Lucas Lopes <lucas.lopes@scorpion.co>
2023-06-21 13:39:16 -0300

    Setup configurations file

---
+++ config/config.py
@@ -0,0 +1,4 @@
+class ProjectConfiguration:
+    WAIT_TIMEOUT = '30'
+    FIREFOX_EXECUTABLE_PATH = '/Applications/Firefox.app/Contents/MacOS/firefox'
+    BASE_URL = 'http://seubarriga.wcaquino.me'
